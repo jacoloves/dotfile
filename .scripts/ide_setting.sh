24b75668068ca389acce86384fb89113e463690b
jacoloves <5511068t@gmail.com>
2022-11-15 23:39:17 +0900

    ide_setting.sh modified

--- .scripts/ide_setting.sh
+++ .scripts/ide_setting.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# past ide-settings
+#if [ "$#" -eq 0 ]; then
+#    tmux split-window -v
+#    tmux split-window -h
+#    tmux resize-pane -D 14
+#    tmux clock-mode
+#    tmux select-pane -t 0
+#fi
+
+# current ide-settings
 if [ "$#" -eq 0 ]; then
-    tmux split-window -v
     tmux split-window -h
+    tmux resize-pane -R 15
+    tmux split-window -v
     tmux resize-pane -D 14
     tmux clock-mode
     tmux select-pane -t 0
