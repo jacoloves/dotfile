1708b7dfb11a54fbb4c4271c43ae0aea5c94f227
jacoloves <5511068t@gmail.com>
2022-11-15 23:50:52 +0900

    ide_setting.sh modified

--- .scripts/ide_setting.sh
+++ .scripts/ide_setting.sh
@@ -12,7 +12,7 @@
 # current ide-settings
 if [ "$#" -eq 0 ]; then
     tmux split-window -h
-    tmux resize-pane -R 15
+    tmux resize-pane -R 35
     tmux split-window -v
     tmux resize-pane -D 14
     tmux clock-mode
