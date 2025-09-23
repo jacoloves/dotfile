a4abdf89c5ae46686d48bee6e29e9ee387f455c0
shooonng <5511068t@gmail.com>
2025-09-23 13:31:11 +0900

    feat: update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("colorscheme nightfox")
+vim.g.lazyvim_check_order = false
+vim.cmd("highlight Normal guibg=NONE")
+vim.cmd("highlight NormalNC guibg=NONE")
