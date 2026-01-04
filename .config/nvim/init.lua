f9f1e20625b67a2b4ab99bcde52306b4c482ecf9
shooonng <5511068t@gmail.com>
2026-01-04 10:39:58 +0900

    feat: update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("colorscheme nightfox")
+vim.cmd("colorscheme gruvbox")
 vim.g.lazyvim_check_order = false
 vim.cmd("highlight Normal guibg=NONE")
 vim.cmd("highlight NormalNC guibg=NONE")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
